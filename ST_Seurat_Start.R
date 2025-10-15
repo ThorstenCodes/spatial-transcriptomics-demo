6b4048f461df66e92699f40b93793324a62dce3d
Thorsten Kaltenbacher <thorsten.kaltenbacher@gmail.com>
2025-10-15 15:49:34 +0200

    first steps with Seurat

--- ST_Seurat_Start.R
+++ ST_Seurat_Start.R
@@ -1,3 +1,35 @@
 # First install renv allowing to create virtual environments in R
 install.packages("renv")
+# Initate the environment 
 renv::init()
+
+# Install some other packages
+install.packages("Seurat")
+install.packages("tidyverse")
+#install.packages("igraph")
+#install.packages("rlang")
+#install.packages("devtools")
+#install.packages("BiocManager")
+#BiocManager::install(c('SingleCellExperiment', 'Summarized Experiment', 'sparseMatrixStats'))
+#install.packages("lsa")
+
+################################################################################
+################################# Libraries ####################################
+################################################################################
+
+library(Seurat)
+library(tidyverse)
+
+
+################################################################################
+################################# Load Data ####################################
+################################################################################
+
+object = readRDS("data/SeuratObj_withTranscripts (1).RDS")
+
+################################################################################
+################################# Exploration ##################################
+################################################################################
+
+object@meta.data
+
